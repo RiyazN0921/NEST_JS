a265122e2e8e2b39adfe2367ed6b0e27dd14d5cb
RiyazN0921 <riyazn886@gmail.com>
2024-04-22 14:20:12 +0530

    controller setup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get()
+    findAll() {
+        return []
+    }
+
+    @Get('query')
+    findAlls(@Query('role') role?: "ENGINEER" | "ADMIN" | "SUPER_ADMIN") {
+        return { role }
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return { id }
+    }
+
+    @Post()
+    create(@Body() body: {}) {
+        return { body }
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body: {}) {
+        return { id, body }
+    }
+
+    @Delete(':id')
+    deleteOne(@Param('id') id: string) {
+        return { id }
+    }
+}
