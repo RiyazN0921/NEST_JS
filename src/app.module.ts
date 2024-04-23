0838a6f7fdb301b4c47068d8de3873cb679e470f
RiyazN0921 <riyazn886@gmail.com>
2024-04-23 15:17:21 +0530

    mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, TasksModule],
+  imports: [UsersModule, MongooseModule.forRoot(""), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
